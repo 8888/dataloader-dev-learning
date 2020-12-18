66b465864da2cf121213595bf7688c0aa2f69958
8888 <leecostello1@gmail.com>
2020-12-17 20:59:35 -0500

    apollo server setup

--- v1/index.js
+++ v1/index.js
@@ -1,5 +1,6 @@
-const { gql } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server');
 
+// GraphQL schema
 const typeDefs = gql`
   type Animal {
     id: ID!
@@ -17,4 +18,25 @@ const typeDefs = gql`
     animal: Animal!
     shelter: Shelter!
   }
+
+  type Query {
+    animals: [Animal]!
+    shelters: [Shelter]!
+    pets: [Pet]!
+  }
 `;
+
+// GraphQL resolvers
+const resolvers = {
+  Query: {
+    animals: () => {},
+    shelters: () => {},
+    pets: () => {},
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🐕🐈 server ready at ${url}`);
+});
